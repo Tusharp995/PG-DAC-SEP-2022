b3c92696e9d3639aec76641f133a40a2988874f9
Tusharp995 <113437110+Tusharp995@users.noreply.github.com>
2022-10-05 22:55:59 +0530

    Add files via upload

---
+++ assignment 2/type1/PatternQ5.java
@@ -0,0 +1,32 @@
+class PatternQ5
+{
+public static void main (String args[])
+{
+for (int i=1;i<=5;i++)
+{
+ for (int j=1;j<=i;j++)
+ {
+	 if ( i==1)
+	 {
+		  System.out.print("A");
+	 }
+	 if ( i==2)
+	 {
+		  System.out.print("B");
+	 }if ( i==3)
+	 {
+		  System.out.print("C");
+	 }if ( i==4)
+	 {
+		  System.out.print("D");
+	 }if ( i==5)
+	 {
+		  System.out.print("E");
+	 }
+ }
+	
+	System.out.println();
+	}
+	
+	}
+	}
